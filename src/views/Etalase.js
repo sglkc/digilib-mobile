edaa4929eda21728990f32093ddc239a91570a60
Rafli <muhammad.rafli241103@gmail.com>
2022-08-15 21:11:39 +0700

    folder_structure_15082022

---
+++ src/views/Etalase.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Etalase() {
+  return (
+    <View>
+      <Text>
+        This is Etalase
+      </Text>
+    </View>
+  );
+}
