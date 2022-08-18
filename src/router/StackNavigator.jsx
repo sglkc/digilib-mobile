74c660f750605cce69960fe4a1d6e3c1f0a3876b
sglkc <31957516+sglkc@users.noreply.github.com>
2022-08-18 19:23:00 +0700

    add notification view

--- src/router/StackNavigator.jsx
+++ src/router/StackNavigator.jsx
@@ -1,10 +1,17 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import {
-  SafeAreaView, Text, View, StyleSheet, ImageBackground, TouchableWithoutFeedback, Keyboard
+  SafeAreaView,
+  Text,
+  View,
+  StyleSheet,
+  ImageBackground,
+  TouchableWithoutFeedback,
+  Keyboard
 } from 'react-native';
 import Login from '@/views/Login';
 import Register from '@/views/Register';
 import Etalase from '@/views/Etalase';
+import Notifikasi from '@/views/Notifikasi';
 
 const style = StyleSheet.create({
   container: {
@@ -40,6 +47,19 @@ const EtalaseScreen = () => {
   );
 }
 
+const NotifikasiScreen = () => {
+  return (
+    <SafeAreaView>
+      <ImageBackground
+        source={require('assets/BG_ORANGE.png')}
+        style={{width: '100%', height: '100%'}}
+      >
+        <Notifikasi />
+      </ImageBackground>
+    </SafeAreaView>
+  );
+}
+
 const Stack = createNativeStackNavigator();
 
 export const StackNavigator = ()=>{
@@ -60,6 +80,11 @@ export const StackNavigator = ()=>{
       <Stack.Screen
         name='Etalase'
         component={EtalaseScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name='Notifikasi'
+        component={NotifikasiScreen}
         options={{ headerShown: false }}
       />
     </Stack.Navigator>
