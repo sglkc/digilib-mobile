386ed9d971d07db80129f41e3ed0c919e1e1540a
sglkc <31957516+sglkc@users.noreply.github.com>
2022-08-22 08:52:30 +0700

    style bottom navbar component

---
+++ src/components/BottomNavbar.jsx
@@ -0,0 +1,50 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+export default function BottomNavbar() {
+  const navigation = useNavigation();
+  const items = [
+    { name: 'Etalase', icon: 'home', route: 'Etalase' },
+    { name: 'Jelajahi', icon: 'folder-open-o', route: 'Jelajahi' },
+    { name: 'Tandai', icon: 'bookmark-o', route: 'Tandai' },
+  ];
+
+  return (
+    <View style={styles.container}>
+      { items.map((item, index) => (
+        <View key={index} style={styles.item}>
+          <Pressable
+            android_ripple={{ color: '#ddd5', borderless: true, radius: 64 }}
+            onPress={() => navigation.navigate(item.route)}
+          >
+            <Icon name={item.icon} size={25} color="white" style={styles.icon}/>
+            <Text style={styles.text}>{ item.name }</Text>
+          </Pressable>
+        </View>
+      )
+      )}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    backgroundColor: 'green',
+    width: '75%',
+    borderTopLeftRadius: 16,
+    borderTopRightRadius: 16,
+    flexDirection:'row',
+    justifyContent:'space-between',
+  },
+  text: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  icon: {
+    marginBottom: 2,
+    alignSelf: 'center',
+  }
+});
