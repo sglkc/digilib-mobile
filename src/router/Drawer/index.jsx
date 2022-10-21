2da93e361a489a88431ac4c09f6abeb1b9efe24c
sglkc <31957516+sglkc@users.noreply.github.com>
2022-10-21 07:45:17 +0700

    feat: add hidden drawer routes

--- src/router/Drawer/index.jsx
+++ src/router/Drawer/index.jsx
@@ -1,89 +1,113 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import Detail from '@/views/Detail';
+import DetailItem from '@/views/DetailItem';
 import Etalase from '@/views/Etalase';
-import Notifikasi from '@/views/Notifikasi';
 import InformasiAkun from '@/views/InformasiAkun';
+import Jelajahi from '@/views/Jelajahi';
+import Kategori from '@/views/Kategori';
+import Notifikasi from '@/views/Notifikasi';
+import Pencarian from '@/views/Pencarian';
 import Riwayat from '@/views/Riwayat';
+import UbahInformasiAkun from '@/views/UbahInformasiAkun';
+import UbahPassword from '@/views/UbahPassword';
 import UmpanBalik from '@/views/UmpanBalik';
+import Tandai from '@/views/Tandai';
 import Tentang from '@/views/Tentang';
+import PDFReader from '@/components/DetailItem/PDFReader';
 import DrawerContent from './DrawerContent';
 import Header from './Header';
 
 const Drawer = createDrawerNavigator();
-const screens = [
-  {
-    name: 'Etalase',
-    component: Etalase,
-    options: {
-      drawerItemStyle: { display: 'none' },
-      hideBanner: true,
-      hideTitle: true,
-      menuButton: true,
-      searchButton: true
-    }
-  },
-  {
-    name: 'Informasi Akun',
-    component: InformasiAkun,
-    options: {
-      expand: true,
-      drawerIcon: () => <Icon name="account-outline" size={25} color="black" />,
-      headerStyle: {
-        backgroundColor: 'transparent',
-        boxShadow: 'none'
+const screens = {
+  shown: [
+    {
+      name: 'Etalase',
+      component: Etalase,
+      options: {
+        drawerItemStyle: { display: 'none' },
+        hideBanner: true,
+        hideTitle: true,
+        menuButton: true,
+        searchButton: true
       }
-    }
-  },
-  {
-    name: 'Notifikasi',
-    component: Notifikasi,
-    options: {
-      expand: true,
-      drawerIcon: () => <Icon name="bell-outline" size={25} color="black" />
-    }
-  },
-  {
-    name: 'Riwayat',
-    component: Riwayat,
-    options: {
-      drawerIcon: () => <Icon name="history" size={25} color="black" />,
-      searchButton: true,
-    }
-  },
-  {
-    name: 'Divider',
-    component: Etalase,
-    options: {
-      drawerItemStyle: {
-        marginHorizontal: -8,
-        marginVertical: 16,
-        height: 1,
-        backgroundColor: 'grey',
+    },
+    {
+      name: 'Informasi Akun',
+      component: InformasiAkun,
+      options: {
+        expand: true,
+        drawerIcon: () => (
+          <Icon name="account-outline" size={25} color="black" />
+        ),
+        headerStyle: {
+          backgroundColor: 'transparent',
+          boxShadow: 'none'
+        }
       }
-    }
-  },
-  {
-    name: 'Umpan Balik',
-    component: UmpanBalik,
-    options: {
-      expand: true,
-      drawerIcon: () => (
-        <Icon name="message-processing-outline" size={25} color="black" />
-      )
-    }
-  },
-  {
-    name: 'Tentang Aplikasi',
-    component: Tentang,
-    options: {
-      expand: true,
-      drawerLabel: 'Tentang',
-      drawerIcon: () => (
-        <Icon name="information-outline" size={25} color="black" />
-      )
-    }
-  },
-];
+    },
+    {
+      name: 'Notifikasi',
+      component: Notifikasi,
+      options: {
+        expand: true,
+        drawerIcon: () => <Icon name="bell-outline" size={25} color="black" />
+      }
+    },
+    {
+      name: 'Riwayat',
+      component: Riwayat,
+      options: {
+        drawerIcon: () => <Icon name="history" size={25} color="black" />,
+        searchButton: true,
+      }
+    },
+    {
+      name: 'Divider',
+      component: Etalase,
+      options: {
+        drawerItemStyle: {
+          marginHorizontal: -8,
+          marginVertical: 16,
+          height: 1,
+          backgroundColor: 'grey',
+        }
+      }
+    },
+    {
+      name: 'Umpan Balik',
+      component: UmpanBalik,
+      options: {
+        expand: true,
+        drawerIcon: () => (
+          <Icon name="message-processing-outline" size={25} color="black" />
+        )
+      }
+    },
+    {
+      name: 'Tentang Aplikasi',
+      component: Tentang,
+      options: {
+        expand: true,
+        drawerLabel: 'Tentang',
+        drawerIcon: () => (
+          <Icon name="information-outline" size={25} color="black" />
+        )
+      }
+    },
+  ],
+  hidden: [
+    { name: 'Detail', component: Detail },
+    { name: 'DetailItem', component: DetailItem },
+    { name: 'Jelajahi', component: Jelajahi },
+    { name: 'Kategori', component: Kategori },
+    { name: 'Pencarian', component: Pencarian },
+    { name: 'PDF Reader', component: PDFReader },
+    { name: 'Tandai', component: Tandai },
+    { name: 'Ubah Informasi Akun', component: UbahInformasiAkun },
+    { name: 'Ubah Password', component: UbahPassword },
+  ]
+};
 
 export default function DrawerNavigator() {
   return (
@@ -101,7 +125,22 @@ export default function DrawerNavigator() {
         header: Header
       }}
     >
-      { screens.map((props, key) => <Drawer.Screen key={key} {...props} />) }
+      { screens.shown.map((props, key) => (
+        <Drawer.Screen key={key} {...props} />
+      ))
+      }
+      { screens.hidden.map((props, key) => (
+        <Drawer.Screen
+          key={key}
+          options={{
+            drawerItemStyle: {
+              display: 'none'
+            }
+          }}
+          {...props}
+        />
+      ))
+      }
     </Drawer.Navigator>
   );
 }
