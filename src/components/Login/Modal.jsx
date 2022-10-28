4af18344c5c72d38166e1d9419881faaebaee335
sglkc <31957516+sglkc@users.noreply.github.com>
2022-10-28 21:12:04 +0700

    feat: implement api to Login/Modal component

--- src/components/Login/Modal.jsx
+++ src/components/Login/Modal.jsx
@@ -1,10 +1,41 @@
+import { useRef, useState } from 'react';
 import {
   StyleSheet, Pressable, Text, TextInput, View
 } from 'react-native';
 import Modal from 'react-native-modal';
+import Alert from '@/components/Alert';
 import Button from '@/components/Button';
+import Axios from '@/func/Axios';
 
-export default function (props) {
+export default function LoginModal(props) {
+  const [alert, setAlert] = useState(null);
+  const email = useRef('');
+  const sent = useRef(false);
+
+  function submit() {
+    if (!email.current) return setAlert({ text: 'Mohon isi email Anda' });
+    if (sent.current) return setAlert({ text: 'Silahkan coba lagi lain kali' });
+
+    Axios.post('/user/password', { email: email.current })
+      .then((res) => {
+        sent.current = true;
+        setAlert({
+          background: '#198754',
+          icon: 'check',
+          text: 'Silahkan cek email Anda untuk mendapatkan kata sandi baru'
+        });
+      })
+      .catch((err) => {
+        if (err.message === 'EMAIL_NOT_FOUND') {
+          return setAlert({
+            text: `Alamat email ${email.current} tidak dapat ditemukan`
+          });
+        }
+
+        setAlert({ text: 'Terjadi error, silahkan coba lagi nanti' });
+      });
+  }
+
   return (
     <View>
       <Modal
@@ -19,10 +50,16 @@ export default function (props) {
           <Text style={styles.title}>Lupa Kata Sandi?</Text>
           <Text style={styles.text}>
             Masukkan alamat email Anda dan kami akan membagikan
-            tautan untuk membuat Kata Sandi baru
+            Anda Kata Sandi baru
           </Text>
-          <TextInput placeholder="Email" style={styles.input} />
-          <Button>Kirim Sekarang</Button>
+          <TextInput
+            placeholder="Email"
+            style={styles.input}
+            onChangeText={(val) => (email.current = val)}
+            onSubmitEditing={submit}
+          />
+          { alert && <Alert style={styles.alert} {...alert} /> }
+          <Button onPress={submit}>Kirim Sekarang</Button>
         </View>
       </Modal>
     </View>
@@ -68,6 +105,9 @@ const styles = StyleSheet.create({
     borderRadius: 32,
     width: '100%',
     backgroundColor: 'orange',
+  },
+  alert: {
+    marginBottom: 26,
   },
   buttonText: {
     fontSize: 18,
